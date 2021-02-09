bd307f855db575299809bdebed2c679e4ea9d1a9
Valerii Lider <valerii.lider@gmail.com>
2021-02-10 00:19:36 +0100

    dsig updated to generate IIC IICSignature

--- internal/signer/signer.go
+++ internal/signer/signer.go
@@ -5,6 +5,8 @@ import (
 	"crypto/x509"
 	"dsig/internal/signedxml"
 	"encoding/base64"
+	"fmt"
+	"strconv"
 
 	"github.com/beevik/etree"
 )
@@ -14,10 +16,19 @@ type Cert interface {
 	GetCertificate() (x509.Certificate, error)
 }
 
+// PKCS1v15Signer represents an interface of signer capable of signing with pkcs1v15
+type PKCS1v15Signer interface {
+	SignPKCS1v15(data []byte) ([]byte, error)
+}
+
 // Signer represents an interface of signer
 type Signer struct {
-	Sig crypto.Signer
-	Cer Cert
+	Sig            crypto.Signer
+	Cer            Cert
+	PKCS1c15Signer PKCS1v15Signer
+	SoftCode       string
+	BusinUnitCode  string
+	TCRCode        string
 }
 
 // Sign signs given xml
@@ -26,6 +37,10 @@ func (s *Signer) Sign(doc *etree.Document) (*etree.Document, error) {
 	// Fill in certificate and subject of signature element
 	cert, err := s.Cer.GetCertificate()
 	if err != nil {
+		return nil, err
+	}
+
+	if err := s.setIIC(doc); err != nil {
 		return nil, err
 	}
 
@@ -58,4 +73,48 @@ func (s *Signer) Sign(doc *etree.Document) (*etree.Document, error) {
 		return nil, err
 	}
 	return doc, nil
+}
+
+func (s *Signer) setIIC(doc *etree.Document) error {
+
+	TIN := doc.FindElement("//Seller").SelectAttr("IDNum").Value
+	IssueDateTime := doc.FindElement("//Invoice").SelectAttr("IssueDateTime").Value
+	ordNum, err := strconv.ParseInt(doc.FindElement("//Invoice").SelectAttr("InvOrdNum").Value, 10, 64)
+	InvOrdNum := fmt.Sprintf("%d", ordNum)
+	TotPrice := doc.FindElement("//Invoice").SelectAttr("TotPrice").Value
+	IICString := fmt.Sprintf(
+		"%s|%s|%s|%s|%s|%s|%s",
+		TIN,
+		IssueDateTime,
+		InvOrdNum,
+		s.BusinUnitCode,
+		s.TCRCode,
+		s.SoftCode,
+		TotPrice,
+	)
+	hasher := crypto.SHA256.New()
+
+	_, err = hasher.Write([]byte(IICString))
+	if err != nil {
+		return err
+	}
+	hashedIIC := hasher.Sum(nil)
+	IICSignature, err := s.PKCS1c15Signer.SignPKCS1v15(hashedIIC)
+	if err != nil {
+		return err
+	}
+	md5Hasher := crypto.MD5.New()
+	_, err = md5Hasher.Write([]byte(IICSignature))
+	if err != nil {
+		return err
+	}
+	IIC := md5Hasher.Sum(nil)
+
+	doc.FindElement("//Invoice").RemoveAttr("IIC")
+	doc.FindElement("//Invoice").CreateAttr("IIC", fmt.Sprintf("%x", IIC))
+
+	doc.FindElement("//Invoice").RemoveAttr("IICSignature")
+	doc.FindElement("//Invoice").CreateAttr("IICSignature", fmt.Sprintf("%x", IICSignature))
+
+	return nil
 }
--- main.go
+++ main.go
@@ -12,18 +12,35 @@ import (
 	"github.com/beevik/etree"
 )
 
+const (
+	// BusinUnitCode represent registered Business unit code
+	BusinUnitCode = ""
+	// SoftCode represents software code
+	SoftCode = "ge788jm169"
+	TCRCode  = ""
+)
+
 func main() {
 
 	pinPtr := flag.String("pin", "", "(Required) Pin is used for unlocking SafeNet token.")
+	// businUnitPtr := flag.String("busin_unit", "", "(Required) Business unit code.")
 	libPtr := flag.String("lib", "", "(Optional) Path to the SafeNet library, eg \"C:\\Windows\\System32\\eTPKCS11.dll\"")
 	var xmlPtr *string
 
 	info, _ := os.Stdin.Stat()
 	if (info.Mode() & os.ModeCharDevice) == os.ModeCharDevice {
-		fmt.Println("dsig command is intended to work with pipes.")
-		fmt.Println("Usage:")
-		fmt.Println("\tcat yourfile.xml | dsig -pin=<your_pin>")
-		os.Exit(1)
+		xmlPtr := flag.String("xml", "", "(Required) Pin is used for unlocking SafeNet token.")
+		buf, err := ioutil.ReadFile(*xmlPtr)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		tmp := string(buf)
+		xmlPtr = &tmp
+		// fmt.Println("dsig command is intended to work with pipes.")
+		// fmt.Println("Usage:")
+		// fmt.Println("\tcat yourfile.xml | dsig -pin=<your_pin>")
+		// os.Exit(1)
 	} else if info.Size() > 0 {
 		reader := bufio.NewReader(os.Stdin)
 		buf, err := ioutil.ReadAll(reader)
@@ -33,8 +50,19 @@ func main() {
 		}
 		tmp := string(buf)
 		xmlPtr = &tmp
+		flag.Parse()
+	} else {
+		xmlPtr = flag.String("xml", "", "(Required) Pin is used for unlocking SafeNet token.")
+		flag.Parse()
+		buf, err := ioutil.ReadFile(*xmlPtr)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		tmp := string(buf)
+		xmlPtr = &tmp
 	}
-	flag.Parse()
+	// flag.Parse()
 
 	if !flag.Parsed() {
 		flag.PrintDefaults()
@@ -64,10 +92,19 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%v\n", err.Error())
 		os.Exit(1)
 	}
+	xmlDoc, err = envelope(xmlDoc)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err.Error())
+		os.Exit(1)
+	}
 
 	signer := &signer.Signer{
-		Sig: dSigToken,
-		Cer: dSigToken,
+		Sig:            dSigToken,
+		Cer:            dSigToken,
+		PKCS1c15Signer: dSigToken,
+		SoftCode:       SoftCode,
+		BusinUnitCode:  BusinUnitCode,
+		TCRCode:        TCRCode,
 	}
 	signedXML, err := signer.Sign(xmlDoc)
 	if err != nil {
@@ -82,3 +119,59 @@ func main() {
 	fmt.Fprintf(os.Stdout, signedXMLStr)
 	os.Exit(0)
 }
+
+func envelope(req *etree.Document) (*etree.Document, error) {
+	doc := etree.NewDocument()
+	doc.IndentTabs()
+
+	root := doc.CreateElement("s:Envelope")
+	root.CreateAttr("xmlns:s", "http://schemas.xmlsoap.org/soap/envelope/")
+	body := root.CreateElement("s:Body")
+	body.CreateAttr("xmlns:xsd", "http://www.w3.org/2001/XMLSchema")
+	body.CreateAttr("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+	tmp := req.FindElement("RegisterInvoiceRequest")
+	body.AddChild(tmp)
+
+	return doc, nil
+}
+
+// func setIIC(invoice *efi.Invoice, BusinUnitCode string) error {
+
+// 	TCRCode := ""
+// 	IICString := fmt.Sprintf(
+// 		"%s|%s|%s|%s|%s|%s|%s",
+// 		invoice.Seller.IDNum,
+// 		time.Time(invoice.IssueDateTime).Format(time.RFC3339),
+// 		fmt.Sprintf("%d", invoice.InvOrdNum),
+// 		BusinUnitCode,
+// 		TCRCode,
+// 		"ge788jm169",
+// 		fmt.Sprintf("%.02f", invoice.TotPrice),
+// 	)
+// 	hasher := crypto.SHA256.New()
+
+// 	_, err := hasher.Write([]byte(IICString))
+// 	if err != nil {
+// 		rec.Error = err.Error()
+// 		return err
+// 	}
+// 	hashedIIC := hasher.Sum(nil)
+// 	IICSignature, err := i.Tkn.SignPKCS1v15(hashedIIC)
+// 	if err != nil {
+// 		rec.Error = err.Error()
+// 		return err
+// 	}
+// 	md5Hasher := crypto.MD5.New()
+// 	_, err = md5Hasher.Write([]byte(IICSignature))
+// 	if err != nil {
+// 		rec.Error = err.Error()
+// 		return err
+// 	}
+// 	IIC := md5Hasher.Sum(nil)
+
+// 	inv.IIC = fmt.Sprintf("%x", IIC)
+// 	inv.IICSignature = fmt.Sprintf("%x", IICSignature)
+
+// 	return nil
+// }
